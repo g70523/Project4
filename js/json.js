bab6434f3f26c066b4900eefc3f45532b5048080
Louise Whitney <louisewhitney@fullsail.edu>
2013-05-31 22:31:49 -0400

    Project 4

---
+++ js/json.js
@@ -0,0 +1,58 @@
+/*  Louise Whitney
+ *  VFW Project 4
+ *  1305
+*/
+var json = {
+    "userInfo1": {
+        "fname": ["Users First Name", "Louise"],
+        "lname": ["Users Last Name", "Whitney"],
+        "email": ["Users Email Address", "lwhitney@gmail.com"],
+        "telephone": ["Users Telephone Number", "123-456-7890"],
+        "usersSex": ["This User is a", "female"],
+        "trips": ["Amount of user Trips", "8"],
+        "preference": ["Users Prefered Method of Travel is", "Flying"],
+        "input": ["Users favorite spots are", "Fiji"],
+        "date": ["Users Next Travel Date is", "June 3, 2013"],
+        "length": ["Users Travel Length is", "2 Nights"]
+    },
+    "userInfo2": {
+        "fname": ["Users First Name", "Josh"],
+        "lname": ["Users Last Name", "Whitney"],
+        "email": ["Users Email Address", "jwhitney@gmail.com"],
+        "telephone": ["Users Telephone Number", "123-456-7890"],
+        "usersSex": ["This User is a", "male"],
+        "trips": ["Amount of user Trips", "4"],
+        "preference": ["Users Prefered Method of Travel is", "none"],
+        "input": ["Users favorite spots are", "Jamaica"],
+        "date": ["Users Next Travel Date is", "June 3, 2013"],
+        "length": ["Users Travel Length is", "One Week"]
+    },
+    "userInfo3": {
+        "fname": ["Users First Name", "William"],
+        "lname": ["Users Last Name", "Torres"],
+        "email": ["Users Email Address", "wtorres@gmail.com"],
+        "telephone": ["Users Telephone Number", "123-456-7890"],
+        "usersSex": ["This User is a", "male"],
+        "trips": ["Amount of user Trips", "9"],
+        "preference": ["Users Prefered Method of Travel is", "Flying"],
+        "input": ["Users favorite spots are", "Mexico"],
+        "date": ["Users Next Travel Date is", "April 3, 2014"],
+        "length": ["Users Travel Length is", "Two Weeks"]
+    },
+    "userInfo4": {
+        "fname": ["Users First Name", "Patty"],
+        "lname": ["Users Last Name", "Stroupe"],
+        "email": ["Users Email Address", "pstroupe@gmail.com"],
+        "telephone": ["Users Telephone Number", "123-456-7890"],
+        "usersSex": ["This User is a", "female"],
+        "trips": ["Amount of user Trips", "1"],
+        "preference": ["Users Prefered Method of Travel is", "Flying"],
+        "input": ["Users favorite spots are", "Bermuda"],
+        "date": ["Users Next Travel Date is", "December 3, 2013"],
+        "length": ["Users Travel Length is", "3 Nights"]
+    }
+    
+}
+
+
+
